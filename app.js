5ece53b7bfb9180b0f09fdf0dd0c74b3cc1b7bb0
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-13 11:01:52 +0200

    Add logic that checks if user's number matches CPU's one and prints the right message

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 // CPU random number generator;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
+
+// User input 'Check' logic
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number! ";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low!";
+  }
+});
